e64d3a733a07d9ce28389f2ca321c09a0df0cd25
dibeligor <dibeligor@gmail.com>
2025-12-01 19:32:50 +0800

    added ID so it matches in the JS

--- Frontend/homepage.php
+++ Frontend/homepage.php
@@ -51,7 +51,7 @@
 
      <!-- Form -->
      <section class="submit-file" id="submitFile" style="display: none;">
-        <form action="../BackEnd/Controller.php?method_finder=create"  method="post">
+        <form action="../BackEnd/Controller.php?method_finder=create"  method="post" id="submitFileForm">
             <div class="form-row">
                 <label for="last_name">Last Name:</label>
                 <input type="text" id="last_name" name="last_name" required>
@@ -143,7 +143,7 @@
             </tr>
             
         </thead>
-        <tbody>
+        <tbody id="files-table">
             <?php
                 $controller = new Controller();
 
@@ -188,31 +188,30 @@
     </table>
 
     <script src="Users/filesData.js"></script>
+    <script>
+        // Javascript to show and hide the form
+            const showForm = document.getElementById("showForm");
+            const submitFile = document.getElementById("submitFile");
     
+            showForm.addEventListener("click", () => {
+                if (submitFile.style.display === "none") {
+                    submitFile.style.display = "block";
+                } else {
+                    submitFile.style.display = "none";
+                }
+            });
+    
+        // Javascript to show and hide the search Bar
+            const showSearch = document.getElementById("showSearch");
+            const searchBar = document.getElementById("searchBar");
+    
+            showSearch.addEventListener("click", () => {
+                if (searchBar.style.display === "none") {
+                    searchBar.style.display = "block";
+                } else {
+                    searchBar.style.display = "none";
+                }
+            });
+    </script>
 </body>
-<script>
-    // Javascript to show and hide the form
-        const showForm = document.getElementById("showForm");
-        const submitFile = document.getElementById("submitFile");
-
-        showForm.addEventListener("click", () => {
-            if (submitFile.style.display === "none") {
-                submitFile.style.display = "block";
-            } else {
-                submitFile.style.display = "none";
-            }
-        });
-
-    // Javascript to show and hide the search Bar
-        const showSearch = document.getElementById("showSearch");
-        const searchBar = document.getElementById("searchBar");
-
-        showSearch.addEventListener("click", () => {
-            if (searchBar.style.display === "none") {
-                searchBar.style.display = "block";
-            } else {
-                searchBar.style.display = "none";
-            }
-        });
-</script>
 </html>
